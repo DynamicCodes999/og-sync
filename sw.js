d5c093b4147b6ab62564f384c1db885fbb8b5f4b
TheGrover295 <william.bertolini@protonmail.com>
2026-08-27 18:58:08 -0500

    Animate navigation and tighten layout

--- sw.js
+++ sw.js
@@ -1,5 +1,5 @@
-const CACHE = "daymark-v12";
-const FILES = ["./", "./index.html", "./styles.css?v=12", "./sync.js?v=12", "./school.js?v=12", "./app.js?v=12", "./icon.svg", "./manifest.webmanifest"];
+const CACHE = "daymark-v14";
+const FILES = ["./", "./index.html", "./styles.css?v=14", "./sync.js?v=14", "./school.js?v=14", "./app.js?v=14", "./icon.svg", "./manifest.webmanifest"];
 
 self.addEventListener("install", event => event.waitUntil(caches.open(CACHE).then(cache => cache.addAll(FILES)).then(() => self.skipWaiting())));
 self.addEventListener("activate", event => event.waitUntil(caches.keys().then(keys => Promise.all(keys.filter(key => key !== CACHE).map(key => caches.delete(key)))).then(() => self.clients.claim())));
